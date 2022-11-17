5d38228c976892ab12624d90954a1c3a2588ed03
Aimé Malcorps <aime.malcorps@neuf.fr>
2022-11-17 10:04:57 +0100

    Backend Calls

---
+++ src/app/services/backend.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+  configUrl = 'http://localhost:4200/api';
+  httpHeaders = new HttpHeaders();
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<any>(this.configUrl + '/login', {email: email, password: password});
+  }
+
+  createAccount(name: string, email: string, password: string, phoneNumber: number) {
+    return this.http.post<any>(this.configUrl + '/create/account', {name,email,password,phoneNumber});
+  }
+
+  getUserConnections(user: User) {
+    return this.http.get<any>(this.configUrl + '/user/connections/' + user.name);
+  }
+
+  getUserPayments(user: User) {
+    return this.http.get<any>(this.configUrl + '/user/payments/' + user.name);
+  }
+
+  sendMoney(user: User, connectionId: number, description: string, amount: number) {
+    return this.http.post<any>(this.configUrl + '/send/money', {user: user, connectionId: connectionId, description: description, amount: amount});
+  }
+
+  addConnection(userId: number, connectionName: string) {
+    return this.http.post<any>(this.configUrl + '/connection/add/' + userId, connectionName);
+  }
+
+}
