c4f28b08a4c87a8acd86fc86c7f962994222a010
Aimé Malcorps <aime.malcorps@neuf.fr>
2022-11-17 10:05:27 +0100

    Home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Payment } from '../models/payment';
+import { User } from '../models/user';
+import { BackendService } from '../services/backend.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  transferForm: FormGroup;
+  user: User = JSON.parse(localStorage.getItem('userDTO')!);
+  connections: User[];
+  payments: Payment[];
+
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private backendService: BackendService) {
+    this.backendService.getUserConnections(this.user)
+      .subscribe((res) => {
+        this.connections = res;
+      })
+    this.backendService.getUserPayments(this.user)
+      .subscribe((res) => {
+        this.payments = res;
+      });
+    this.generateForm();
+  }
+
+  ngOnInit(): void {
+  }
+
+  generateForm() {
+    this.transferForm = this.fb.group({
+      'connection': this.fb.control(''),
+      'description': this.fb.control(''),
+      'amount': this.fb.control('')
+    }, { updateOn: 'submit' });
+  }
+
+  onSubmit() {
+    console.log(this.transferForm.value.connection);
+    console.log(this.transferForm.value.description);
+    console.log(this.transferForm.value.amount);
+
+    this.backendService.sendMoney(this.user, this.transferForm.value.connection.id,
+      this.transferForm.value.description, this.transferForm.value.amount)
+      .subscribe(res => {
+        if (res != null) {
+          console.log(res);
+          this.backendService.getUserPayments(this.user)
+            .subscribe((res) => {
+              this.payments = res;
+            })
+        }
+      })
+  }
+
+  selectConnection(conncetion: any) {
+    this.transferForm.controls['connection'].setValue(conncetion);
+  }
+
+  displayFnConnection(connectionName: string) {
+    return connectionName;
+  }
+
+  trackPaymentList() {
+    return this.payments ? this.payments : undefined;
+  }
+
+  addConnection() {
+    this.router.navigate(['/add/connection/', this.connections]);
+  }
+
+}
