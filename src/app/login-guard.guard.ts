0fd11496ff486953d1ce4bd46095057cf9fef5a9
Aimé Malcorps <aime.malcorps@neuf.fr>
2022-11-25 11:09:10 +0100

    Guard

---
+++ src/app/login-guard.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from './models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuardGuard implements CanActivate {
+  constructor(private router: Router) {
+    
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    const user: User = JSON.parse(localStorage.getItem('userDTO') as string);
+
+    if (user == null) {
+      this.router.navigate(['/login']);
+    }
+    
+    return true;
+  }
+
+}
